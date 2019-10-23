0bbc74093c98ce3c666972a66423d52a16a02258
Kotan Tamas <33610984+torta211@users.noreply.github.com>
2019-10-23 21:36:18 +0200

    delete backend folder

--- s2e-backend/moneyengine/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MoneyengineConfig(AppConfig):
-    name = 'moneyengine'
--- s2e-backend/moneyengine/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import Transaction
-
-class TransactionSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Transaction
-        fields = ('receiverID', 'senderID', 'amount', 'message', 'status')
