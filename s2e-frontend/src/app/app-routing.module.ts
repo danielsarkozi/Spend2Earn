31d43e67bc983dddd4c71e45dcb92f274987857b
Tamas Laczik <tamas.laczik@transferwise.com>
2019-10-23 20:03:33 +0200

    Added new routes

--- s2e-frontend/src/app/app-routing.module.ts
+++ s2e-frontend/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { LandingComponent } from "./landing/landing.component";
+import { PersonalDetailsComponent } from "./register/personal-details/personal-details.component";
+import { BankAccountsComponent } from "./register/bank-accounts/bank-accounts.component";
+import { AgreementComponent } from "./register/agreement/agreement.component";
 
 const routes: Routes = [
     { path: "", redirectTo: "/landing", pathMatch: "full" },
     { path: "home", component: HomeComponent },
     { path: "login", component: LoginComponent },
-    { path: "landing", component: LandingComponent }
+    { path: "landing", component: LandingComponent },
+    { path: "register", component: PersonalDetailsComponent },
+    { path: "bank-accounts", component: BankAccountsComponent },
+    { path: "agreement", component: AgreementComponent }
 ];
 
 @NgModule({
