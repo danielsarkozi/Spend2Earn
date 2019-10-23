850ce4925e367465a38137174127b3f0bcb01806
danielsarkozi <daniel.sarkozi@bca.hu>
2019-10-23 18:16:37 +0200

    Added Transaction data table

--- moneyengine/models.py
+++ moneyengine/models.py
@@ -1,25 +1,6 @@
 from django.db import models
 from enum import Enum, auto
 
-class Transaction(models.Model):
-    '''
-    receiverID = models.IntegerField()
-    senderID = models.IntegerField()
-    amount = models.IntegerField()
-    message = models.CharField(max_length=110)
-    status = models.CharField(max_length=20)
-
-    '''
-    transaction_id = models.AutoField(primary_key=True)
-    payee = models.ForeignKey(User)
-    payer = models.ForeignKey(User)
-    amount = models.PositiveIntegerField()
-    savings = models.PositiveIntegerField()
-    status = payer = models.CharField( choices=Status.choices() )
-
-    def __str__(self):
-        return self.status + ": " + self.amount + "$ from " + self.receiverID + ", to " + self.senderID
-
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
 
@@ -33,4 +14,25 @@ class Status(Enum):
     @classmethod
     def choices( cls ):
         return tuple( ( i.name, i.value ) for i in cls )
+
+class Transaction(models.Model):
+    '''
+    receiverID = models.IntegerField()
+    senderID = models.IntegerField()
+    amount = models.IntegerField()
+    message = models.CharField(max_length=110)
+    status = models.CharField(max_length=20)
+
+    '''
+    transaction_id = models.AutoField(primary_key=True)
+    payee = models.ForeignKey(User, on_delete = models.CASCADE)
+    payer = models.ForeignKey(User, on_delete = models.CASCADE)
+    amount = models.PositiveIntegerField()
+    savings = models.PositiveIntegerField()
+    status = payer = models.CharField( choices=Status.choices() )
+
+    def __str__(self):
+        pass
+        #return self.status + ": " + self.amount + "$ from " + self.receiverID + ", to " + self.senderID
+
 
