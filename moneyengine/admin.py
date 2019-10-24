046cdaca1944af4c27a1aba3f3d1330ce49016de
botagergo <43295162+botagergo@gmail.com>
2019-10-24 11:12:26 +0200

    new models: Iban, Card

--- moneyengine/admin.py
+++ moneyengine/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Transaction, AlternativeUser, TransactionStatusChange
+from .models import Transaction, AlternativeUser, TransactionStatusChange, Iban, Card
 
 admin.site.register(Transaction)
 admin.site.register(AlternativeUser)
 admin.site.register(TransactionStatusChange)
+admin.site.register(Iban)
+admin.site.register(Card)
