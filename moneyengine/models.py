a452db9d8179e336231c6f96a59813a1f9efab2f
Kotan Tamas <33610984+torta211@users.noreply.github.com>
2019-10-24 19:54:00 +0200

    transactions stored by iban

--- moneyengine/models.py
+++ moneyengine/models.py
@@ -21,36 +21,40 @@ class AlternativeUser(models.Model):
     user_registration_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return "(" + self.user_id + ") " + self.username
+        return "(" + str(self.user_id) + ") " + self.user_name
 
 class Session(models.Model):
     session_id = models.CharField(max_length=60, primary_key=True)
-    user =models.ForeignKey(AlternativeUser, on_delete=models.CASCADE, related_name='%(class)s_userofsession')
+    user = models.ForeignKey(AlternativeUser, on_delete=models.CASCADE, related_name='%(class)s_userofsession')
+    creation = models.DateTimeField(auto_now=True)
+
+class Iban(models.Model):
+    iban_id = models.AutoField(primary_key=True)
+    account_owner = models.CharField(max_length=255)
+    alias = models.CharField(max_length=255)
+    country = models.CharField(max_length=2)
+    check_digit = models.CharField(max_length=2)
+    bank = models.IntegerField()
+    number = models.CharField(max_length=30)
+    owner = models.ForeignKey(AlternativeUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.account_owner.user_name) + "'s " + self.alias + " account"
 
 class Transaction(models.Model):
     transaction_id = models.AutoField(primary_key=True)
-    payee = models.ForeignKey(AlternativeUser, on_delete = models.CASCADE, related_name='%(class)s_payee')
-    payer = models.ForeignKey(AlternativeUser, on_delete = models.CASCADE, related_name='%(class)s_payer')
+    source_iban = models.ForeignKey(Iban, on_delete = models.CASCADE, related_name='%(class)s_payeriban')
+    destination_iban = models.ForeignKey(Iban, on_delete = models.CASCADE, related_name='%(class)s_payeeiban')
     amount = models.DecimalField(decimal_places=2, max_digits=14)
     savings = models.DecimalField(decimal_places=2, max_digits=14)
 
     def __str__(self):
-        return "transaction no. " + self.transaction_id + ": " + self.amount + "$ from " + self.payer + ", to " + self.payee
+        return str(self.transaction_id) + ": " + str(self.amount) + "$ from " + str(self.source_iban) + " to " + str(self.destination_iban)
 
 class TransactionStatusChange(models.Model):
     subject_transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE, related_name='%(class)s_subjecttransaction')
     new_status = models.CharField(max_length=255, choices=Status.choices())
     timestamp = models.DateTimeField(auto_now=True)
-
-class Iban(models.Model):
-	iban_id = models.AutoField(primary_key=True)
-	account_owner = models.CharField(max_length=255)
-	alias = models.CharField(max_length=255)
-	country = models.CharField(max_length=2)
-	check_digit = models.CharField(max_length=2)
-	bank = models.IntegerField()
-	number = models.CharField(max_length=30)
-	owner = models.ForeignKey(AlternativeUser, on_delete=models.CASCADE)
 
 class Card(models.Model):
 	number = models.CharField(max_length=255)
