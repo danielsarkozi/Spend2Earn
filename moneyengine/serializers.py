3654eeaa2d6b899b8abae3f4676fd7ba4fe0539e
Kotan Tamas <33610984+torta211@users.noreply.github.com>
2019-11-05 13:57:45 +0100

    another try

--- moneyengine/serializers.py
+++ moneyengine/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Transaction, TransactionStatusChange, Iban, Card
 
-class UserSerializer(serializers.Serializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
