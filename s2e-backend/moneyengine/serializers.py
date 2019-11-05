bd7ccdf08f5783cbf71d7032310b6ec8ee6eea5f
Kotan Tamas <33610984+torta211@users.noreply.github.com>
2019-11-06 00:13:24 +0100

    REMAKE

--- s2e-backend/moneyengine/serializers.py
+++ s2e-backend/moneyengine/serializers.py
@@ -1,29 +1,18 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
-from .models import Transaction, TransactionStatusChange, Iban, Card, Profile
+from .models import Transaction, TransactionStatusChange, Iban, Card, CustomUser
 
-class ProfileSerializer(serializers.ModelSerializer):
-    pin = serializers.IntegerField()
-
-    class Meta:
-        model = Profile
-        fields = ('pin',)
-
-class UserSerializer(serializers.ModelSerializer):
+class CustomUserSerializer(serializers.HyperlinkedModelSerializer):
     password = serializers.CharField(write_only=True)
-    profile = ProfileSerializer(required=False)
 
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email', 'username', 'password', 'userprofile')
-
-
-    def create(self, validated_data, instance=None):
-        profile_data = validated_data.pop('userprofile')
-        user = User.objects.create(**validated_data)
+    def create(self, validated_data):
+        user = CustomUser.objects.create(username=validated_data['username'])
         user.set_password(validated_data['password'])
         user.save()
-        Profile.objects.update_or_create(user=user,**profile_data)
+        return user
+
+    class Meta:
+        model = CustomUser
+        fields = ['url', 'pin', 'password', 'username', 'email', 'is_staff']
 
 class TransactionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
