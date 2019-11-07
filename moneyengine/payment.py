90e0cc8cb1980a9b73a5f45d21eeec83ea9cc2c5
botagergo <43295162+botagergo@gmail.com>
2019-11-07 11:20:20 +0100

    payment mock class added with unit tests

---
+++ moneyengine/payment.py
@@ -0,0 +1,59 @@
+import random
+from .models import Transaction, TransactionStatusChange, Iban, Status
+
+class Payment:
+    def __init__(self, source_iban, destination_iban, amount):
+        if amount < 0:
+            raise Exception("amount must be nonnegative")
+
+        self.transaction = Transaction(
+            source_iban = source_iban,
+            destination_iban = destination_iban,
+            amount = amount,
+            savings = 0
+        )
+        self.transaction.save()
+
+        TransactionStatusChange(
+            subject_transaction = self.transaction,
+            new_status = Status.created
+        ).save()
+
+    def status(self):
+        return TransactionStatusChange.objects \
+            .filter(subject_transaction = self.transaction) \
+            .latest('timestamp') \
+            .new_status
+
+    def updateStatus(self, status):
+        TransactionStatusChange(
+            subject_transaction = self.transaction,
+            new_status = status
+        ).save()
+
+    def validatePayer(self):
+        TransactionStatusChange(
+            subject_transaction = self.transaction,
+            new_status = Status.approved_by_payer
+        ).save()
+        return True
+
+    def makePayment(self):
+        status = self.status()
+        if status in {Status.created, Status.denied_by_payer}:
+            raise Exception('payer not validated')
+        if status == Status.approved_by_bank:
+            raise Exception('request already approved by bank')
+
+        success = random.choices([True, False], [0.95, 0.05])
+        if success:
+            status = Status.approved_by_bank
+        else:
+            status = Status.denied_by_bank
+
+        TransactionStatusChange(
+            subject_transaction = self.transaction,
+            new_status = status
+        ).save()
+
+        return success
