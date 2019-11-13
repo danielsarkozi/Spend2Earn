33a0a01a6d7cdbb38a1b7128c742d518fff5b4e1
Tamás Laczik <tamas.laczik@transferwise.com>
2019-11-13 20:52:40 +0100

    Remove DropdownModule

--- s2e-frontend/src/app/app.module.ts
+++ s2e-frontend/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
-import { DropDownModule } from "nativescript-drop-down/angular";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -24,8 +23,7 @@ import { TerminalComponent } from "./terminal/terminal.component";
     imports: [
         NativeScriptModule,
         AppRoutingModule,
-        NativeScriptFormsModule,
-        DropDownModule
+        NativeScriptFormsModule
     ],
     declarations: [
         AppComponent,
