f0d0f0026d1a8fd061a6d8f14b7ba5172c2482dd
Tamás Laczik <tamas.laczik@transferwise.com>
2019-12-03 22:35:35 +0100

    Added Finances to side navigation

--- s2e-frontend/src/app/finances/finances.component.ts
+++ s2e-frontend/src/app/finances/finances.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BankAccount } from '../interfaces';
 
 @Component({
   selector: 'Finances',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinancesComponent implements OnInit {
 
-  text = "You got 17$, nigga!"
+  private bankAccounts: BankAccount[] = [];
 
   constructor() { }
 
