6c520a49be746ede7c6db0f4608c455f9a5dc00a
Tamás Laczik <tamas.laczik@transferwise.com>
2019-12-06 01:13:39 +0100

    Wired up history

--- s2e-frontend/src/app/history/history.component.ts
+++ s2e-frontend/src/app/history/history.component.ts
@@ -9,21 +9,27 @@ import { TransactionView } from './transaction'
 })
 export class HistoryComponent implements OnInit {
 
-  transaction_list = [new TransactionView( 'asd', 'dsa', 12, 2, 'huf' ),
-                      new TransactionView( 'asd', 'dsa', 12, 2, 'huf' ),
-                      new TransactionView( 'asd', 'dsa', 12, 2, 'huf' )]
+  transaction_list = []
 
   constructor(private userService: UserService) { }
 
   async ngOnInit() {
-    /*
     var ret = await this.userService.getTransactions();
-    ret.forEach(element => {
-      var tr = new TransactionView( element["source_iban"].split(": ")[1], element["destination_iban"].split(": ")[1], element["amount"], element["savings"], element["currency"] )
+    ret.forEach((element: any) => {
+      var tr = new TransactionView( /*element["source_iban"].split(": ")[1], element["destination_iban"].split(": ")[1]*/ element["destination_account_owner"], element["destination_alias"], element["amount"], element["savings"], element["currency"], this.getColor(element.status))
       this.transaction_list.push(tr)
     });
     console.log(this.transaction_list)
-    */
+  }
+
+  private getColor(status: string): string {
+    if(status === 'created') {
+      return 'yellow';
+    }
+    else if(status.includes('denied')) {
+      return 'red';
+    }
+    return 'lightseagreen';
   }
 
 }
--- s2e-frontend/src/app/history/transaction.ts
+++ s2e-frontend/src/app/history/transaction.ts
@@ -6,12 +6,14 @@ export class TransactionView{
     amount: number;
     savings: number;
     currency: string;
+    color: string;
 
-    constructor(source, destination, amount, savings, currency){
+    constructor(source, destination, amount, savings, currency, color){
         this.source_iban = source;
         this.destination_iban = destination;
         this.amount = amount;
         this.savings = savings;
         this.currency = currency;
+        this.color = color;
     }
 }
